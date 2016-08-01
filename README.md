fa41d3b479a175f71e9ed1e8998b1c26844c021b
zhenchy <zhenchaoyang@haizitong.com>
2016-08-01 14:53:43 +0800

    support Bitcode

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Lame
-lame
+Lame官网源码:[https://sourceforge.net/projects/lame/files/lame/](https://sourceforge.net/projects/lame/files/lame/)
 WAV音频文件转码为MP3格式
 从网上找到lame的静态库包，发现不支持64位和模拟器，自己去官网下载了源码后，重新生成flat静态包（支持x86/armv7/arm64），具体可以使用  lipo -info *.a查看。
+
+# Release Note
+* 0.0.1 支持不同架构
+* 0.0.2 支持bitCode
+
---
+++ src/lame-3.99.5/build-lame.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+#默认最低版本为7.0，如需修改，直接修改59行的数字即可
+
+CONFIGURE_FLAGS="--disable-shared --disable-frontend"
+
+ARCHS="arm64 armv7s x86_64 i386 armv7"
+
+# directories
+SOURCE=""
+FAT="fat-lame"
+
+#必须将xxx替换为lame在电脑上的绝对路径,如:/Users/Jack/Desktop/lame-3.99.5，同时将该脚本放在该路径下
+SCRATCH="xxx"
+
+THIN=`pwd`/"thin-lame"
+
+COMPILE="y"
+LIPO="y"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		    if [ "$ARCH" = "x86_64" ]
+		    then
+		    	SIMULATOR="-mios-simulator-version-min=7.0"
+                        HOST=x86_64-apple-darwin
+		    else
+		    	SIMULATOR="-mios-simulator-version-min=5.0"
+                        HOST=i386-apple-darwin
+		    fi
+		else
+		    PLATFORM="iPhoneOS"
+		    SIMULATOR="-mios-simulator-version-min=7.0"
+                    HOST=arm-apple-darwin
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang -arch $ARCH"
+		#AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+		CFLAGS="-arch $ARCH $SIMULATOR"
+		if ! xcodebuild -version | grep "Xcode [1-6]\."
+		then
+			CFLAGS="$CFLAGS -fembed-bitcode"
+		fi
+		CXXFLAGS="$CFLAGS"
+		LDFLAGS="$CFLAGS"
+
+		CC=$CC $CWD/$SOURCE/configure \
+		    $CONFIGURE_FLAGS \
+                    --host=$HOST \
+		    --prefix="$THIN/$ARCH" \
+                    CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
